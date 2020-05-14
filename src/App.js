8006204b71fea68d5060950c40fd832013093749
Ali-Amin <ali.m.amin98@gmail.com>
2020-05-14 14:44:57 +0200

    Add project images

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import './App.css';
+import shopxImg1 from "../src/assets/shopx1.png";
+
 
 function App() {
   return (
     <div class="app">
 
       <div className="personal-info">
-        <div className="sidebar-text">ALI MOHAMED EMAD</div>
+        <div className="name-text">ALI MOHAMED EMAD</div>
+        <div className="profile-description-text">
+          I am a really good developer please hire me
+        </div>
       </div>
 
       <div className="projects">
+        <div className="project-title">ShopX</div>
+        <img className="app-img-left" src={shopxImg1} alt="Logo" />
+        <img className="app-img-right" src={shopxImg1} alt="Logo" />
+        <div className="project-subtitle">Personal Project</div>
+
+        <div className="project-title">Cantina</div>
+        <img className="app-img-left" src={shopxImg1} alt="Logo" />
+        <img className="app-img-right" src={shopxImg1} alt="Logo" />
+        <div className="project-subtitle">Personal Project</div>
+
+        <div className="project-title">Chicken11</div>
+        <img className="app-img-left" src={shopxImg1} alt="Logo" />
+        <img className="app-img-right" src={shopxImg1} alt="Logo" />
+        <div className="project-subtitle">Personal Project</div>
       </div>
+
+
 
     </div>
   );
